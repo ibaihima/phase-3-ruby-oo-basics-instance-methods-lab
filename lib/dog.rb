1fadd05d944f0a374d09f05bf2b05ff1c984858b
Ibaihima <ibaihima@hotmail.com>
2022-03-15 21:26:48 -0400

    commit tree cc6fd6ba0a376e4b6be134ce31b40ba9531ea4e3

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+
+
+class Dog
+    def bark
+        puts "Woof!"
+    end
+    def sit 
+        puts "The Dog is sitting"
+    end
+end
+fido = Dog.new
+puts fido.bark
+puts greg = Dog.new
+puts greg.sit
